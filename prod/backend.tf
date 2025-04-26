165331956c00bd3eb0f8db9632247c94a2034a92
alain-ateh <mrmeloateh@yahoo.com>
2025-04-26 13:35:40 -0400

    modified backend bucket name

--- prod/backend.tf
+++ prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "class38dominion-terraform-backend"
+    bucket         = "class38dominion-terraform-backend3"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
