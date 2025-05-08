8b300faf4514d95f77bec5595fec0a8f698d752b
Miracle Oluwole <miracle@Miracles-Laptop.lan>
2025-05-08 10:49:27 -0600

    modified 1

--- modules/jenkins-server/main.tf
+++ modules/jenkins-server/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "jenkins_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-  vpc_security_group_ids      = var.security_group_id
+  vpc_security_group_ids      = [var.security_group_id]
   subnet_id                   = var.subnet_id
   user_data     = file("${path.module}/jenkins.sh")
 
