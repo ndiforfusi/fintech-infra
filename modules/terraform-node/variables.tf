943be1eb27b984ec596c84b15697ce832d1c8f57
Miracle Oluwole <miracle@Miracles-Laptop.lan>
2025-05-08 15:47:29 -0600

    modify 17

--- modules/terraform-node/variables.tf
+++ modules/terraform-node/variables.tf
@@ -1,7 +1,7 @@
 variable "ami_id" {
   description = "The AMI ID for the Terraform node"
   type        = string
-  default     = ""
+  default     = "ami-04f167a56786e4b09"
 }
 
 variable "instance_type" {
