97dbe081fdc6fed17dad1f552a4329610c9bfa3e
Miracle Oluwole <miracle@Miracles-Laptop.lan>
2025-05-08 16:59:49 -0600

    eks variable.tf

--- modules/eks-client-node/variables.tf
+++ modules/eks-client-node/variables.tf
@@ -1,7 +1,7 @@
 variable "ami_id" {
   description = "The AMI ID for the Terraform node. Leave empty to automatically fetch the latest Ubuntu AMI."
   type        = string
-  default     = "ami-04f167a56786e4b09"
+  default     = ""
 }
 
 variable "instance_type" {
