4164fb201dcb1b7adfe29efa022fc1113f008bfc
alain-ateh <mrmeloateh@yahoo.com>
2025-05-11 20:22:29 -0400

    initial commit

--- prod/backend.tf
+++ prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "class38dominion-terraform-backend2"
+    bucket         = "class38dominion-terraform-backend32"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
