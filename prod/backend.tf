4aef02955c1efa597096ddcda0d7f49420708333
alain-ateh <mrmeloateh@yahoo.com>
2025-06-12 04:21:53 -0400

    edited backend.tf

--- prod/backend.tf
+++ prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "class38-terraform-backend-bucket36"
+    bucket         = "class38-terraform-backend-bucket35"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
