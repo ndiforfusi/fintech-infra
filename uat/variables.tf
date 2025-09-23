579fe16e1c8ee3eff3621c5a0841a06bb0cc03c3
azong B <azongbauclaire@gmail.com>
2025-09-22 20:08:49 -0400

    updated files for class39 bootcamp

--- uat/variables.tf
+++ uat/variables.tf
@@ -20,7 +20,7 @@ variable "cluster_name" {
 
 variable "rolearn" {
   description = "Add admin role to the aws-auth configmap"
-  default     = "arn:aws:iam::999568710647:role/terraform-create-role"
+  default     = "arn:aws:iam::148761670084:role/terraform-create-role"
 }
 
 # ################################################################################
@@ -87,7 +87,7 @@ variable "route53_zone_id" {
 
 variable "aws_account_id" {
   description = "AWS Account ID"
-  default     = "999568710647"
+  default     = "148761670084"
 }
 
 variable "repositories" {
