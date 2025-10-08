991294b0ed1f7eb7635e6ac267075588c7996058
kayode adebayo <kaybabaguv20023@gmail.com>
2025-10-08 08:52:30 +0100

    new changed

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "dev/terraform.state"
+    key            = "dev/terraform.tfstate"
     bucket         = "class390-terraform-backend-bucket"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locking"
