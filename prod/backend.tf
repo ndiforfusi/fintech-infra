0d41a51dbcf01242ec8a54a2e757dd77ee2d98d2
kayode adebayo <kaybabaguv20023@gmail.com>
2025-10-08 08:44:35 +0100

    new change

--- prod/backend.tf
+++ prod/backend.tf
@@ -1,8 +1,9 @@
-cluster_certificate_authority_dataterraform {
-  required_version = ">=0.12.0"
+terraform {
+  required_version = ">= 0.12.0"
+
   backend "s3" {
-    key            = "prod/terraform.tfstate"
     bucket         = "class390-terraform-backend-bucket"
+    key            = "prod/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locking"
   }
