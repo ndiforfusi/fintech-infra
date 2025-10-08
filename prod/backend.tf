d264a018f6fbe750f5ffbb90e405bae4276b30f7
kayode adebayo <kaybabaguv20023@gmail.com>
2025-10-08 07:02:30 +0100

    adjusted

--- prod/backend.tf
+++ prod/backend.tf
@@ -1,7 +1,7 @@
 cluster_certificate_authority_dataterraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "prod/terraform.state"
+    key            = "prod/terraform.tfstate"
     bucket         = "class390-terraform-backend-bucket"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locking"
