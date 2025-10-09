4d49d0851202e3ee00f213b07832620e09b97421
Engr.  Adebayo <199798010+shemphad@users.noreply.github.com>
2025-10-10 00:57:29 +0100

    Update main.tf

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -107,7 +107,7 @@ module "eks" {
   access_entries = {
     fusi = {
       kubernetes_groups = ["eks-admins"]
-      principal_arn     = "arn:aws:iam::999568710647:user/nfusi"
+      principal_arn     = "arn:aws:iam::418272782718:user/general"
       policy_associations = [
         {
           policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
