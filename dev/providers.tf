a0fc875d5104a90dec4719ec8a04299fb2eb2578
nwa1990 <nwachukwuchideraa@yahoo.com>
2025-10-11 21:36:58 -0600

    updated provider.tf

--- dev/providers.tf
+++ dev/providers.tf
@@ -16,7 +16,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  Kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
